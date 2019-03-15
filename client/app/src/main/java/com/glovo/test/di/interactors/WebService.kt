a6f0d022c82af086c1b0ee6b9e964aec339ed2d8
Roman Tolmachev <tolmachevroman@gmail.com>
2019-03-15 20:58:27 -0300

    add API calls to get cities and countries

--- client/app/src/main/java/com/glovo/test/di/interactors/WebService.kt
+++ client/app/src/main/java/com/glovo/test/di/interactors/WebService.kt
@@ -1,5 +1,15 @@
 package com.glovo.test.di.interactors
 
+import com.glovo.test.data.models.City
+import com.glovo.test.data.models.Country
+import io.reactivex.Single
+import retrofit2.http.GET
+
 interface WebService {
 
+    @GET("cities")
+    fun getCities(): Single<List<City>>
+
+    @GET("countries")
+    fun getCountries(): Single<List<Country>>
 }
