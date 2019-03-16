58e3eb266e08165f040e6800385842becf51ab92
Roman Tolmachev <tolmachevroman@gmail.com>
2019-03-15 21:49:27 -0300

    add generic Response class for handling web response cases

---
+++ client/app/src/main/java/com/glovo/test/di/interactors/Response.kt
@@ -0,0 +1,20 @@
+package com.glovo.test.di.interactors
+
+
+class Response<out T> constructor(val status: Status, val data: T?, val error: Throwable?) {
+    enum class Status {
+        SUCCESS, ERROR, LOADING
+    }
+
+    companion object {
+
+        fun <T> success(data: T?): Response<T> = Response(Status.SUCCESS, data, null)
+
+        fun <T> error(data: T? = null, error: Throwable?): Response<T> = Response(Status.ERROR, data, error)
+
+        fun <T> loading(data: T?): Response<T> = Response(Status.LOADING, data, null)
+    }
+
+    override fun toString(): String =
+        status.toString() + ", data:" + data?.toString() + ", error:" + error.toString()
+}
