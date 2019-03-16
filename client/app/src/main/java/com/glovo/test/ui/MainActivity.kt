99818e7a49d66cf971076606295e7d3ba1a3cb57
Roman Tolmachev <tolmachevroman@gmail.com>
2019-03-16 19:32:15 -0300

    refactor flows

--- client/app/src/main/java/com/glovo/test/ui/MainActivity.kt
+++ client/app/src/main/java/com/glovo/test/ui/MainActivity.kt
@@ -3,23 +3,23 @@ package com.glovo.test.ui
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Criteria
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
-import android.os.Handler
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.glovo.test.R
 import com.glovo.test.common.permissions.PermissionUtils
+import com.glovo.test.common.rx.ResponseObserver
 import com.glovo.test.data.models.City
 import com.glovo.test.di.interactors.Response
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -27,18 +27,22 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.PolygonOptions
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
-import com.google.android.gms.maps.model.PolygonOptions
-import com.google.maps.android.PolyUtil
 import kotlinx.android.synthetic.main.content_main.*
+import javax.inject.Inject
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     companion object {
         const val MY_LOCATION_REQUEST_CODE = 101
+    }
+
+    private val overlayColor by lazy {
+        ResourcesCompat.getColor(resources, R.color.polygonFillColor, null)
     }
 
     private var googleMap: GoogleMap? = null
@@ -57,54 +61,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mainViewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
         mainViewModel.apply {
 
-            citiesResponse.observe(this@MainActivity, Observer { response ->
-                when (response.status) {
-                    Response.Status.SUCCESS -> {
-
-                        //TODO compare working areas of newly arrived CityWithPolygons with my location
-                        // and drawPolygonOptions for my location only
-                        response.data?.let { cityWithPolygonOptions ->
-                            val myLocation = getMyLocation()
-                            val myLatLng = LatLng(myLocation.latitude, myLocation.longitude)
-
-                            //TODO refactor!!
-                            val points = mutableListOf<LatLng>()
-                            cityWithPolygonOptions.polygonOptionsAvailable.forEach {
-                                points.addAll(it.points)
-                            }
-
-//                            println("Got city ${cityWithPolygonOptions.name}")
-                            val iAmInArea = PolyUtil.containsLocation(myLatLng, points, true)
-                            if (iAmInArea) {
-                                println("I am in the area of ${cityWithPolygonOptions.name}")
-                                drawPolygonOptions(cityWithPolygonOptions.polygonOptionsAvailable)
-                                mainViewModel.getCityByCode(cityWithPolygonOptions.code)
-                            }
-                        }
-
-                    }
-                    Response.Status.ERROR -> {
-
-                    }
-                    Response.Status.LOADING -> {
-
-                    }
+            /**
+             * Observes City details (currency, language code...) request
+             */
+            cityDetailsByCodeResponse.observe(this@MainActivity, ResponseObserver(this@MainActivity.localClassName,
+                onSuccess = { city ->
+                    showCityDetails(city)
                 }
-            })
+            ))
 
-            cityByCodeResponse.observe(this@MainActivity, Observer { response ->
-                when (response.status) {
-                    Response.Status.SUCCESS -> {
-                        response.data?.also { showCityInformation(it) }
-                    }
-                    Response.Status.ERROR -> {
-
-                    }
-                    Response.Status.LOADING -> {
-
-                    }
-                }
-            })
+            /**
+             * Observes decoded list of working areas and draws them on map
+             */
+            workingAreasDecodedResponse.observe(
+                this@MainActivity, ResponseObserver(this@MainActivity.localClassName,
+                    onSuccess = { workingAreasDecoded ->
+                        drawWorkingAreaDecoded(workingAreasDecoded)
+                    })
+            )
         }
 
         (supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?)?.also { mapFragment ->
@@ -134,12 +108,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             return
         }
 
-        if (PermissionUtils.isPermissionGranted(
-                permissions, grantResults,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        ) {
-            // Enable the my location layer if the permission has been granted.
+        /**
+         * Load cities and check if any contains the current location, or manually select a city
+         */
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
             enableMyLocation()
         } else {
             showSelectCityFragment()
@@ -151,48 +123,74 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             != PackageManager.PERMISSION_GRANTED
         ) {
             // Permission to access the location is missing.
-            PermissionUtils.requestPermission(
-                this, MY_LOCATION_REQUEST_CODE,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
+            PermissionUtils.requestPermission(this, MY_LOCATION_REQUEST_CODE, Manifest.permission.ACCESS_FINE_LOCATION)
         } else if (googleMap != null) {
             // Access to the location has been granted to the app.
             googleMap?.isMyLocationEnabled = true
 
-            zoomToLocation(getMyLocation())
-
-            mainViewModel.getCities()
+            getMyLocation()?.also { currentLocation ->
+                val currentCoordinate = LatLng(currentLocation.latitude, currentLocation.longitude)
+                zoomToCoordinate(currentCoordinate)
+                mainViewModel.getCityByCurrentLocation(currentCoordinate)
+            }
         }
     }
 
-    //TODO add permissions and case when user selected manually
-    @SuppressLint("MissingPermission")
-    private fun getMyLocation(): Location {
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val criteria = Criteria()
-        val provider = locationManager.getBestProvider(criteria, true)
-        return locationManager.getLastKnownLocation(provider)
+    /**
+     * Obtain my current location if permissions are given
+     */
+    private fun getMyLocation(): Location? {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val criteria = Criteria()
+            val provider = locationManager.getBestProvider(criteria, true)
+            return locationManager.getLastKnownLocation(provider)
+        }
+
+        return null
     }
 
-    private fun zoomToLocation(location: Location) {
-        val coordinate = LatLng(location.latitude, location.longitude)
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 14f))
+    private fun zoomToCoordinate(coordinate: LatLng) {
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinate, 13f))
     }
 
-    private fun drawPolygonOptions(polygonOptionsAvailable: List<PolygonOptions>) {
-        polygonOptionsAvailable.forEach { polygonOption ->
+    /**
+     * Draw Polygons based on decoded points from City working area, find the center of the whole
+     * region and zoom to the center coordinate
+     */
+    private fun drawWorkingAreaDecoded(workingAreaDecoded: List<MainViewModel.WorkingAreaDecoded>) {
 
-            Handler().postDelayed({
-                googleMap?.addPolygon(polygonOption)
-            }, 200)
+        googleMap?.clear()
 
+        // calculate center of working area
+        val builder = LatLngBounds.Builder()
+
+        workingAreaDecoded.forEach { area ->
+            if (area.points.isNotEmpty()) {
+                val polygonOptions = PolygonOptions()
+                    .strokeWidth(1.0f)
+                    .fillColor(overlayColor)
+                    .addAll(area.points)
+                googleMap?.addPolygon(polygonOptions)
+
+                area.points.forEach { builder.include(it) }
+            }
+        }
+
+        val bounds: LatLngBounds = builder.build()
+
+        // zoom to the center
+        bounds.center.also {
+            zoomToCoordinate(it)
         }
     }
 
-    private fun showCityInformation(city: City) {
-        cityInformationTextView.visibility = View.VISIBLE
-        cityInformationTextView.text = getString(
-            R.string.city_information,
+    private fun showCityDetails(city: City) {
+        cityDetailsTextView.visibility = View.VISIBLE
+        cityDetailsTextView.text = getString(
+            R.string.city_details,
             city.name,
             city.countryCode,
             city.timeZone,
@@ -203,10 +201,13 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun showSelectCityFragment() {
         val selectCityFragment = SelectCityFragment()
-        selectCityFragment.show(supportFragmentManager, "selectCity")
+        selectCityFragment.show(supportFragmentManager, "SelectCityFragment")
     }
 
+    /**
+     * Direct method of getting city code back from the SelectCityFragment
+     */
     fun onCitySelected(cityCode: String) {
-        println("City selected was $cityCode")
+        mainViewModel.getCityByCode(cityCode, decodeArea = true)
     }
 }
--- client/app/src/main/java/com/glovo/test/ui/MainViewModel.kt
+++ client/app/src/main/java/com/glovo/test/ui/MainViewModel.kt
@@ -1,20 +1,14 @@
 package com.glovo.test.ui
 
-import android.content.Context
-import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.glovo.test.R
 import com.glovo.test.data.models.City
-import com.glovo.test.data.models.Country
 import com.glovo.test.data.repositories.CitiesRepository
-import com.glovo.test.data.repositories.CountriesRepository
 import com.glovo.test.di.interactors.Response
 import com.glovo.test.di.modules.Callback
 import com.glovo.test.di.modules.IoThreads
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Polygon
-import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.maps.android.PolyUtil
 import io.reactivex.Observable
 import io.reactivex.Scheduler
@@ -25,84 +19,92 @@ import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-    private val context: Context,
     private val citiesRepository: CitiesRepository,
     @IoThreads private val ioScheduler: Scheduler,
     @Callback private val callbackScheduler: Scheduler
 ) : ViewModel() {
 
-    private val overlayColor by lazy {
-        ResourcesCompat.getColor(context.resources, R.color.polygonFillColor, null)
-    }
-
     private val disposable: CompositeDisposable = CompositeDisposable()
 
-    val citiesResponse: MutableLiveData<Response<CityWithPolygonOptions>> = MutableLiveData()
-    val cityByCodeResponse: MutableLiveData<Response<City>> = MutableLiveData()
+    val cityDetailsByCodeResponse: MutableLiveData<Response<City>> = MutableLiveData()
+    val workingAreasDecodedResponse: MutableLiveData<Response<List<WorkingAreaDecoded>>> = MutableLiveData()
 
-    fun getCities() {
+    fun getCityByCurrentLocation(currentCoordinate: LatLng) {
         citiesRepository.getCities()
             .subscribeOn(ioScheduler)
             .observeOn(ioScheduler)
             .subscribeBy(onSuccess = { cities ->
 
                 Observable.fromIterable(cities)
-                    .flatMap {
-                        Single.just(buildCityWithPolygonOptions(it))
+                    .flatMap { city ->
+
+                        val builder = LatLngBounds.Builder()
+
+                        val workingAreasDecoded = mutableListOf<WorkingAreaDecoded>()
+                        city.workingArea.forEach { areaEncoded ->
+                            val pointsDecoded = PolyUtil.decode(areaEncoded)
+                            workingAreasDecoded.add(WorkingAreaDecoded(pointsDecoded))
+                            pointsDecoded.forEach { builder.include(it) }
+                        }
+                        val bounds: LatLngBounds = builder.build()
+                        val containsCurrentCoordinate = bounds.contains(currentCoordinate)
+
+                        Single.just(Triple(workingAreasDecoded, containsCurrentCoordinate, city))
                             .toObservable()
                     }
                     .subscribeOn(ioScheduler)
                     .observeOn(callbackScheduler)
-                    .subscribeBy(onNext = { cityWithPolygonOptions ->
-                        // we add new cities one by one
-                        citiesResponse.value = Response.success(cityWithPolygonOptions)
+                    .subscribeBy(onNext = { (areas, containsCurrentCoordinate, city) ->
+                        // only if city contains my current coordinates
+                        if (containsCurrentCoordinate) {
+                            workingAreasDecodedResponse.value = Response.success(areas)
+                            getCityByCode(city.code, decodeArea = false)
+                        }
                     }, onError = {
                         //TODO
                     })
 
             }, onError = {
-                citiesResponse.value = Response.error(data = null, error = it)
+                workingAreasDecodedResponse.value = Response.error(data = null, error = it)
             }).addTo(disposable)
     }
 
-    fun getCityByCode(code: String) {
+    fun getCityByCode(code: String, decodeArea: Boolean) {
         citiesRepository.getCityByCode(code)
             .subscribeOn(ioScheduler)
             .observeOn(callbackScheduler)
             .subscribeBy(onSuccess = { city ->
-                cityByCodeResponse.value = Response.success(city)
+                cityDetailsByCodeResponse.value = Response.success(city)
+                if (decodeArea) {
+                    decodeWorkingArea(city.workingArea)
+                }
             }, onError = {
-                citiesResponse.value = Response.error(data = null, error = it)
+                cityDetailsByCodeResponse.value = Response.error(data = null, error = it)
             }).addTo(disposable)
     }
 
-    private fun buildCityWithPolygonOptions(city: City): CityWithPolygonOptions {
-        val polygonOptionsAvailable = mutableListOf<PolygonOptions>()
-        city.workingArea.forEach { area ->
-            val pointsInArea = PolyUtil.decode(area)
-            if (pointsInArea.isNotEmpty()) {
-                polygonOptionsAvailable.add(
-                    PolygonOptions()
-                        .strokeWidth(1.0f)
-                        .fillColor(overlayColor)
-                        .addAll(pointsInArea)
-                )
+    private fun decodeWorkingArea(workingArea: List<String>) {
+        Single.just(workingArea)
+            .map {
+                val workingAreasDecoded = mutableListOf<WorkingAreaDecoded>()
+                workingArea.forEach { areaEncoded ->
+                    workingAreasDecoded.add(WorkingAreaDecoded(PolyUtil.decode(areaEncoded)))
+                }
+                workingAreasDecoded
             }
-        }
-        return CityWithPolygonOptions(
-            name = city.name,
-            code = city.code,
-            polygonOptionsAvailable = polygonOptionsAvailable
-        )
+            .subscribeOn(ioScheduler)
+            .observeOn(callbackScheduler)
+            .subscribeBy(onSuccess = {
+                workingAreasDecodedResponse.value = Response.success(it)
+            }, onError = {
+                //TODO
+            }).addTo(disposable)
     }
+
 
     override fun onCleared() {
         disposable.clear()
     }
 
-    data class CityWithPolygonOptions(
-        val name: String,
-        val code: String,
-        val polygonOptionsAvailable: List<PolygonOptions>
-    )
+    data class WorkingAreaDecoded(val points: List<LatLng>)
 }
