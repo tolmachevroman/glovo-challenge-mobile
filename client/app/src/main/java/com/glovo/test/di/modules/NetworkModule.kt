362865cdd7f1c667be36150ddad729a7e977173e
Roman Tolmachev <tolmachevroman@gmail.com>
2019-03-15 21:01:10 -0300

    fix RxJava handling in Retrofit

--- client/app/src/main/java/com/glovo/test/di/modules/NetworkModule.kt
+++ client/app/src/main/java/com/glovo/test/di/modules/NetworkModule.kt
@@ -11,6 +11,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
@@ -26,6 +27,7 @@ class NetworkModule(private val baseUrl: String) {
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
             .baseUrl(baseUrl)
             .client(okHttpClient)
