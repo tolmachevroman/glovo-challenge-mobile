a3bd2f491fbff089c2e0d92b63f5369dc06c295f
Roman Tolmachev <tolmachevroman@gmail.com>
2019-03-16 00:29:44 -0300

    add get City by code API

--- client/app/src/main/java/com/glovo/test/di/interactors/WebService.kt
+++ client/app/src/main/java/com/glovo/test/di/interactors/WebService.kt
@@ -4,11 +4,15 @@ import com.glovo.test.data.models.City
 import com.glovo.test.data.models.Country
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface WebService {
 
     @GET("cities")
     fun getCities(): Single<List<City>>
+
+    @GET("cities/{city_code}")
+    fun getCityByCode(@Path("city_code") cityCode: String): Single<City>
 
     @GET("countries")
     fun getCountries(): Single<List<Country>>
