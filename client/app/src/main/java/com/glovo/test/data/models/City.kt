ff38751a99bf5c07784dd8df8413390a2b4dbac6
Roman Tolmachev <tolmachevroman@gmail.com>
2019-03-15 21:48:56 -0300

    fix non camel case fields parsing with Moshi in City data class

--- client/app/src/main/java/com/glovo/test/data/models/City.kt
+++ client/app/src/main/java/com/glovo/test/data/models/City.kt
@@ -1,13 +1,19 @@
 package com.glovo.test.data.models
 
+import com.squareup.moshi.Json
+
 data class City(
     val code: String,
     val name: String,
+    @field:Json(name = "country_code")
     val countryCode: String,
     val currency: String?,
     val enabled: Boolean?,
     val busy: Boolean?,
+    @field:Json(name = "time_zone")
     val timeZone: String?,
+    @field:Json(name = "language_code")
     val languageCode: String?,
+    @field:Json(name = "working_area")
     val workingArea: List<String>
 )
